24d06a3b2f45ae9c1087693179a105ea1f42db23
yoshiharu-Ohkita <yoshi-1218.bat@outlook.jp>
2022-12-10 18:37:09 +0900

    ブログ投稿作成処理実行

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,22 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
-            @csrf
-            <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="post[title]" placeholder="タイトル"/>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class="content">
+            <div class="content__post">
+                <h3>本文</h3>
+                <p>{{ $post->body }}</p>    
             </div>
-            <div class="body">
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
-            </div>
-            <input type="submit" value="store"/>
-        </form>
+        </div>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
