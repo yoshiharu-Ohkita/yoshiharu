e0db1abc84645617a323a29649f8369f5a63e66f
yoshiharu-Ohkita <yoshi-1218.bat@outlook.jp>
2022-12-13 13:38:28 +0900

    ブログ投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
+    }
+    public function show(post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
+        public function create()
+    {
+        return view('posts/create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 
-    public function show(Post $post)
-    {   
-        return view('posts/show')->with(['post' => $post]);
-    } 
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
+        return redirect('/posts/' . $post->id);
+    }
 }
